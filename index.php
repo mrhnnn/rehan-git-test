8baa471f925fb907a49bc2ce31150def2a459818
mrhnnn <raihanpadhopati@gmail.com>
2022-11-15 09:55:23 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
   <h1>Hello World!</h1>
   <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi esse minima aliquam ab debitis beatae cum rem velit quod obcaecati! Expedita possimus necessitatibus ex omnis nisi, iure delectus qui velit!</p>
+  <ul>
+    <li>1</li>
+    <li>2</li>
+  </ul>
 </body>
 </html>
